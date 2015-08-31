3ad333fd82245d537690b8383f3dfabcd6c7c233
John-Titor-2036 <mkurisu4@gmail.com>
2015-08-31 15:19:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ alert("Hey Johnathan!");
 function init(){
     var maidChan = window.open("", "width=200, height=400");
     doc = maidChan.document;
-    var zip = $.get("http://ipinfo.io", function(response) {
-                        console.log(response.postal);
-                    }, "jsonp");
+    var zip = 00000
+    $.get("http://ipinfo.io", function(response) {
+        zip = (response.postal);
+    }, "jsonp");
     maidChan.alert(zip);
 }
