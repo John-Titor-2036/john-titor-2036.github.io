98eb5f17914942ca37141b3aa42e3d5ed405e212
John-Titor-2036 <mkurisu4@gmail.com>
2015-08-31 15:12:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $.get("http://ipinfo.io", function(response) {
     console.log(response.postal);
 }, "jsonp");
 
-doc = maidChan.document;
+function init(){
+    var maidChan = window.open("", "width=200, height=400");
+    doc = maidChan.document;
+}
