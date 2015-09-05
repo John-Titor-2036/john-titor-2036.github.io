74267c4bdad433c9da3bd747f3fd465c9b8e1e9e
John-Titor-2036 <mkurisu4@gmail.com>
2015-09-05 08:58:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ function init(){
     $.get("http://ipinfo.io", function(response) {
         zip = parseInt(response.postal);
         maidChan.alert(zip);
-    }, "jsonp");
-    //maidChan.alert(zip);
+    }, "jsonp"); 
+    getWeather(zip);
+}
+
+function getWeather(zip){
+    $(document).ready(function() {
+        $.simpleWeather({
+        location: zip,
+        woeid: '',
+        unit: 'f',
+        success: function(weather) {
+        html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+  
+            $("#weather").html(html);
+        },
+        error: function(error) {
+            $("#weather").html('<p>'+error+'</p>');
+        }
+    });
+});
 }
