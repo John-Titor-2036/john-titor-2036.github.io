16b4be4e0c3b9acb3b4f837b1d9834088286aff8
John-Titor-2036 <mkurisu4@gmail.com>
2015-09-05 09:04:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,18 @@ function init(){
         zip = parseInt(response.postal);
         maidChan.alert(zip);
     }, "jsonp"); 
-    getWeather(zip);
-}
-
-function getWeather(zip){
     $(document).ready(function() {
         $.simpleWeather({
         location: zip,
         woeid: '',
         unit: 'f',
         success: function(weather) {
-            alert(parseInt(weather.temp));
+            maidChan.alert(parseInt(weather.temp));
         },
         error: function(error) {
-            alert("WEATHER NOT FOUND!");
+            maidChan.alert("WEATHER NOT FOUND!");
         }
+        });
     });
-});
 }
+
