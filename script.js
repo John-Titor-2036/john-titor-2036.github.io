0505f8a946d0e1a4455cb91f7c859e80a9db5dbd
John-Titor-2036 <mkurisu4@gmail.com>
2015-10-04 18:37:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function init(){
     var maidChan = window.open("", "width=200, height=400");
     maidHtml = maidChan.html;
     doc = maidChan.document;
-    var zip = 0;
+    //var zip = 0;
     $.get("http://ipinfo.io", function(response) {
         var zip = parseInt(response.postal);
         alert(zip);
